f63710dfa03839af4432390fc5fcf0ca0b4820a6
Akshara <aksharaa009@gmail.com>
2024-01-16 10:41:07 +0530

    first commit

---
+++ src/org/ict/git/Palindrome.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.ict.git;
+
+/**
+ * 
+ */
+public class Palindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r,sum=0,temp;    
+		  int n=454;//It is the number variable to be checked for palindrome  
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");  
+
+	}
+
+}
